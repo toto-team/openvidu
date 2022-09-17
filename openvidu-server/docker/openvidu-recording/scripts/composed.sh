d562d6e16e11e9dfc52e295247839770d59fd97b
Yasmine <mineyaskonat02@gmail.com>
2022-09-17 14:29:07 +0200

    add facebook ffmpeg cmd

--- openvidu-server/docker/openvidu-recording/scripts/composed.sh
+++ openvidu-server/docker/openvidu-recording/scripts/composed.sh
@@ -32,6 +32,7 @@ fi
   export VIDEO_FORMAT
   export RECORDING_JSON
   export RTMP_YOUTUBE_LINK
+  export RTMP_FACEBOOK_LINK
   echo "==== Loaded Environment Variables ======================="
   env
   echo "========================================================="
@@ -74,9 +75,11 @@ fi
     then
       # Do not record audio
       <./stop ffmpeg -y -f x11grab -draw_mouse 0 -framerate $FRAMERATE -video_size $RESOLUTION -i :$DISPLAY_NUM -c:v libx264 -preset ultrafast -crf 28 -refs 4 -qmin 4 -pix_fmt yuv420p -filter:v fps=$FRAMERATE "/recordings/$VIDEO_ID/$VIDEO_NAME.$VIDEO_FORMAT" -f flv $RTMP_YOUTUBE_LINK
+      <./stop ffmpeg -y -f x11grab -draw_mouse 0 -framerate $FRAMERATE -video_size $RESOLUTION -i :$DISPLAY_NUM -c:v libx264 -preset ultrafast -crf 28 -refs 4 -qmin 4 -pix_fmt yuv420p -filter:v fps=$FRAMERATE "/recordings/$VIDEO_ID/$VIDEO_NAME.$VIDEO_FORMAT" -f flv RTMP_FACEBOOK_LINK
     else
       # Record audio  ("-f alsa -i pulse [...] -c:a aac")
       <./stop ffmpeg -y -f alsa -i pulse -f x11grab -draw_mouse 0 -framerate $FRAMERATE -video_size $RESOLUTION -i :$DISPLAY_NUM -c:a aac -c:v libx264 -preset ultrafast -crf 28 -refs 4 -qmin 4 -pix_fmt yuv420p -filter:v fps=$FRAMERATE "/recordings/$VIDEO_ID/$VIDEO_NAME.$VIDEO_FORMAT" -f flv $RTMP_YOUTUBE_LINK
+      <./stop ffmpeg -y -f alsa -i pulse -f x11grab -draw_mouse 0 -framerate $FRAMERATE -video_size $RESOLUTION -i :$DISPLAY_NUM -c:a aac -c:v libx264 -preset ultrafast -crf 28 -refs 4 -qmin 4 -pix_fmt yuv420p -filter:v fps=$FRAMERATE "/recordings/$VIDEO_ID/$VIDEO_NAME.$VIDEO_FORMAT" -f flv RTMP_FACEBOOK_LINK
   fi
 
   ### Generate video report file ###
